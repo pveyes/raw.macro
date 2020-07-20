2cb873ecde8fd7d3c5e3c0271ed1fa942d708de8
Fatih Kalifa <fatihkalifa@gmail.com>
2020-07-20 10:03:10 +0700

    feat: dynamic require support (#37)

--- README.md
+++ README.md
@@ -4,6 +4,15 @@
 
 > Webpack [`raw-loader`](https://github.com/webpack-contrib/raw-loader) implemented as [`babel-plugin-macros`](https://github.com/kentcdodds/babel-plugin-macros)
 
+## Installation
+
+In order to use raw.macro in your own project, you can use one of the following commands:
+
+```
+$ yarn add --dev raw.macro
+$ npm install --save-dev raw.macro
+```
+
 ## Usage
 
 raw.macro is similar to Node’s `require` call:
@@ -14,23 +23,27 @@ import raw from "raw.macro";
 const markdown = raw("./README.md");
 ```
 
-## Installation
+**Note: Because raw.macro uses babel internally to replace `raw()` calls, your transpiled code won't be changed if you only change the file that you import. This is because from babel perspective, your JS file is unchanged**
 
-In order to use raw.macro in your own project, you can use one of the following commands:
+One workaround that you can do that doesn't involve restarting your build system is making small changes where you put `raw()` calls, for example by adding `console.log()` with different content.
 
+### Dynamic path import
+
+You can also use import dynamic path using [template literal](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals). You can even use them inside a function / React component!
+
+```js
+import raw from "raw.macro";
+
+function Article(props) {
+  const content = raw(`../content/${props.locale}.md`);
+  return <Markdown content={content} />;
+}
 ```
-$ yarn add --dev raw.macro
-$ npm install --save-dev raw.macro
-```
 
-## Why
+**This method has 2 caveats:**
 
-I came across a few problem when using `raw-loader` in `create-react-app`:
-
-* I need to use webpack loader syntax (which needs to be disabled via eslint).
-* Some newlines are removed unintentionally.
-
-This can also be useful in environment where webpack is not available / not extensible. Just use babel and you're good to go.
+1. You can only use up to two variables inside template literal. 1 for directory name, and 1 for file name.
+2. Using dynamic path import will includes all files that matches your dynamic path, which can make your JS bundle a lot bigger. This is also partly the reason of limitation described in #1
 
 ## License
 
--- src/macro.js
+++ src/macro.js
@@ -4,10 +4,18 @@ const { createMacro } = require("babel-plugin-macros");
 
 export default createMacro(rawMacros);
 
+const RAW_DYNAMIC_VARIABLE_NAME_PREFIX = "__raw_dynamic__";
+
 function rawMacros({ references, state, babel }) {
+  // we define counter here so it always starts from 0 for different file
+  // we use object so we can mutate inside requireRaw
+  let usageCounter = {
+    value: 0,
+  };
+
   references.default.forEach(referencePath => {
     if (referencePath.parentPath.type === "CallExpression") {
-      requireRaw({ referencePath, state, babel });
+      requireRaw({ referencePath, state, babel, usageCounter });
     } else {
       throw new Error(
         `This is not supported: \`${referencePath
@@ -18,17 +26,121 @@ function rawMacros({ references, state, babel }) {
   });
 }
 
-function requireRaw({ referencePath, state, babel }) {
+function requireRaw({ referencePath, state, babel, usageCounter }) {
   const filename = state.file.opts.filename;
   const t = babel.types;
   const callExpressionPath = referencePath.parentPath;
   const dirname = path.dirname(filename);
   let rawPath;
 
-  try {
-    rawPath = callExpressionPath.get("arguments")[0].evaluate().value;
-  } catch (err) {
-    // swallow error, print better error below
+  const arg = callExpressionPath.get("arguments")[0];
+
+  switch (arg.node.type) {
+    case "TemplateLiteral": {
+      const { expressions, quasis } = arg.node;
+
+      const isInvalidTemplateLiteral = quasis[0].value.raw === "";
+      if (isInvalidTemplateLiteral) {
+        throw new Error(
+          "Invalid value, variable interpolation can't be at the start of template literal",
+        );
+      }
+
+      if (expressions.length > 2) {
+        throw new Error(
+          "Invalid value. You can only have 2 dynamic values max. 1 for directory name, 1 for file name.",
+        );
+      }
+
+      const dynamicVariableName =
+        RAW_DYNAMIC_VARIABLE_NAME_PREFIX + usageCounter.value;
+      usageCounter.value++;
+
+      const variables = expressions.map((node, idx) => {
+        const isDirectory =
+          idx === quasis.length - 1
+            ? false
+            : arg.node.quasis[idx + 1].value.raw.startsWith("/");
+        return {
+          name: node.name,
+          isDirectory,
+        };
+      });
+
+      const rootDir = path.join(dirname, quasis[0].value.raw);
+      const pathEntries = fs
+        .readdirSync(rootDir)
+        .filter(p => {
+          try {
+            return (
+              fs.lstatSync(path.join(rootDir, p)).isDirectory() ===
+              variables[0].isDirectory
+            );
+          } catch (err) {
+            return false;
+          }
+        })
+        .flatMap(p => {
+          if (!variables[0].isDirectory) {
+            if (p.endsWith(quasis[1].value.raw)) {
+              return p.replace(quasis[1].value.raw, "");
+            } else {
+              return [];
+            }
+          }
+
+          return p;
+        });
+
+      // start from index 1 because index 0 is always base directory
+      quasis.slice(1).forEach((quasi, i) => {
+        const pathFragment = quasi.value.raw;
+        const variable = variables[i];
+
+        if (variable.isDirectory) {
+          pathEntries.forEach((traversedPath, j) => {
+            if (!quasi.tail) {
+              const traversedDirname = path.join(rootDir, traversedPath);
+              const paths = fs.readdirSync(traversedDirname).filter(p => {
+                // quasi loop happen after .slice(1) call which means next quasi is
+                // i+2 instead of i+1
+                return p.endsWith(quasis[i + 2].value.raw);
+              });
+
+              pathEntries[j] = [traversedPath, paths];
+            }
+          });
+        }
+      });
+
+      const programPath = getProgramPath(referencePath);
+      programPath.unshiftContainer(
+        "body",
+        t.variableDeclaration("var", [
+          t.variableDeclarator(
+            t.identifier(dynamicVariableName),
+            createObjectASTFromPathEntries(
+              t,
+              pathEntries,
+              rootDir,
+              quasis[quasis.length - 1].value.raw,
+            ),
+          ),
+        ]),
+      );
+
+      callExpressionPath.replaceWith(
+        t.expressionStatement(
+          variables.reduce((acc, variable) => {
+            return t.memberExpression(acc, t.identifier(variable.name), true);
+          }, t.identifier(dynamicVariableName)),
+        ),
+      );
+      return;
+    }
+    case "StringLiteral":
+      rawPath = arg.node.value;
+      break;
   }
 
   if (rawPath === undefined) {
@@ -41,7 +153,52 @@ function requireRaw({ referencePath, state, babel }) {
   const fullPath = require.resolve(rawPath, { paths: [dirname] });
   const fileContent = fs.readFileSync(fullPath, { encoding: "utf-8" });
 
-  referencePath.parentPath.replaceWith(
+  callExpressionPath.replaceWith(
     t.expressionStatement(t.stringLiteral(fileContent)),
   );
+}
+
+function createObjectASTFromPathEntries(
+  t,
+  pathEntries,
+  rootDir,
+  fileName = "",
+) {
+  return t.objectExpression(
+    pathEntries
+      .map(entry => {
+        if (Array.isArray(entry)) {
+          return t.objectProperty(
+            t.stringLiteral(entry[0]),
+            createObjectASTFromPathEntries(
+              t,
+              entry[1],
+              path.join(rootDir, entry[0]),
+            ),
+          );
+        }
+
+        try {
+          const rawPath = entry + fileName;
+          const fullPath = require.resolve(rawPath, {
+            paths: [rootDir],
+          });
+          return t.objectProperty(
+            t.stringLiteral(entry),
+            t.stringLiteral(fs.readFileSync(fullPath, "utf-8")),
+          );
+        } catch (err) {
+          return null;
+        }
+      })
+      .filter(Boolean),
+  );
+}
+
+function getProgramPath(path) {
+  if (path.parentPath) {
+    return getProgramPath(path.parentPath);
+  }
+
+  return path;
 }
